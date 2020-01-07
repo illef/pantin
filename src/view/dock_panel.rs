9a4d2ceac8fc5993f7ed7328ebbe49aa1f315175
taylor <taylor@jennifersoft.com>
2020-01-07 10:50:32 +0900

    Refactoring dock_panel

--- src/view/dock_panel.rs
+++ src/view/dock_panel.rs
@@ -1,6 +1,4 @@
-use super::utils;
 use super::*;
-use std::sync::Arc;
 
 pub struct DockPanel {
     buffer_cache: Option<Buffer>,
@@ -19,76 +17,116 @@ impl DockPanel {
         self.childs.push((alignment, view))
     }
 
-    fn render_child(&mut self) {
-        let buffer = self.buffer_cache.as_mut().unwrap();
+    fn render_nothing(buffer_mut_view: BufferMutView, _: &mut Box<dyn View>) -> (Point, Size) {
+        (Point(0, 0), buffer_mut_view.size())
+    }
+
+    fn render_right(
+        mut buffer_mut_view: BufferMutView,
+        child_view: &mut Box<dyn View>,
+    ) -> (Point, Size) {
+        let width = child_view.desire_size().width;
+
+        let mut child_mut_view = buffer_mut_view.as_mut_view(
+            Point(buffer_mut_view.size().width - width, 0),
+            Size {
+                width: buffer_mut_view.size().width - width,
+                height: buffer_mut_view.size().height,
+            },
+        );
+        child_view.render(&mut child_mut_view);
+        let offset = Point(0, 0);
+        let size = Size {
+            width: buffer_mut_view.size().width - width,
+            height: buffer_mut_view.size().height,
+        };
+
+        (offset, size)
+    }
+
+    fn render_left(
+        mut buffer_mut_view: BufferMutView,
+        child_view: &mut Box<dyn View>,
+    ) -> (Point, Size) {
+        let width = child_view.desire_size().width;
+
+        let mut child_mut_view = buffer_mut_view.as_mut_view(
+            Point(0, 0),
+            Size {
+                width,
+                height: buffer_mut_view.size().height,
+            },
+        );
+        child_view.render(&mut child_mut_view);
+        let offset = Point(width, 0);
+        let size = Size {
+            width: buffer_mut_view.size().width - width,
+            height: buffer_mut_view.size().height,
+        };
+
+        (offset, size)
+    }
+
+    fn render_bottom(
+        mut buffer_mut_view: BufferMutView,
+        child_view: &mut Box<dyn View>,
+    ) -> (Point, Size) {
+        let height = child_view.desire_size().height;
+        let mut child_mut_view = buffer_mut_view.as_mut_view(
+            Point(0, buffer_mut_view.size().height - height),
+            Size {
+                width: buffer_mut_view.size().width,
+                height: height,
+            },
+        );
+        child_view.render(&mut child_mut_view);
+        let size = Size {
+            width: buffer_mut_view.size().width,
+            height: buffer_mut_view.size().height - height,
+        };
+        (Point(0, 0), size)
+    }
+
+    fn render_top(
+        mut buffer_mut_view: BufferMutView,
+        child_view: &mut Box<dyn View>,
+    ) -> (Point, Size) {
+        let height = child_view.desire_size().height;
+        let mut child_mut_view = buffer_mut_view.as_mut_view(
+            Point(0, 0),
+            Size {
+                width: buffer_mut_view.size().width,
+                height: height,
+            },
+        );
+        child_view.render(&mut child_mut_view);
+        let size = Size {
+            width: buffer_mut_view.size().width,
+            height: buffer_mut_view.size().height - height,
+        };
+        (Point(0, height), size)
+    }
+
+    fn render_child(mut buffer: Buffer, childs: &mut Vec<(Alignment, Box<dyn View>)>) -> Buffer {
         let mut offset = Point(0, 0);
         let mut size = buffer.size();
 
-        for child in self.childs.iter_mut() {
-            if child.0 == Alignment::Left {
-                let mut buffer_mut_view = self
-                    .buffer_cache
-                    .as_mut()
-                    .unwrap()
-                    .as_mut_view(offset, size);
-                let width = child.1.desire_size().width;
+        for child in childs.iter_mut() {
+            let buffer_mut_view = buffer.as_mut_view(offset, size);
+            let func = match child.0 {
+                Alignment::Left => Self::render_left,
+                Alignment::Bottom => Self::render_bottom,
+                Alignment::Top => Self::render_top,
+                Alignment::Right => Self::render_right,
+                _ => Self::render_nothing,
+            };
 
-                let mut child_mut_view = buffer_mut_view.as_mut_view(
-                    offset,
-                    Size {
-                        width,
-                        height: buffer_mut_view.size().height,
-                    },
-                );
-                child.1.render(&mut child_mut_view);
-                offset = offset.add(Point(width, 0));
-                size = Size {
-                    width: size.width - width,
-                    height: size.height,
-                }
-            } else if child.0 == Alignment::Bottom {
-                let mut buffer_mut_view = self
-                    .buffer_cache
-                    .as_mut()
-                    .unwrap()
-                    .as_mut_view(offset, size);
-                let height = child.1.desire_size().height;
-
-                let mut child_mut_view = buffer_mut_view.as_mut_view(
-                    Point(0, size.height - height),
-                    Size {
-                        width: size.width,
-                        height: height,
-                    },
-                );
-                child.1.render(&mut child_mut_view);
-                size = Size {
-                    width: size.width,
-                    height: size.height - height,
-                }
-            } else if child.0 == Alignment::Top {
-                let mut buffer_mut_view = self
-                    .buffer_cache
-                    .as_mut()
-                    .unwrap()
-                    .as_mut_view(offset, size);
-                let height = child.1.desire_size().height;
-
-                let mut child_mut_view = buffer_mut_view.as_mut_view(
-                    Point(0, 0),
-                    Size {
-                        width: size.width,
-                        height: height,
-                    },
-                );
-                child.1.render(&mut child_mut_view);
-                offset = offset.add(Point(0, height));
-                size = Size {
-                    width: size.width,
-                    height: size.height - height,
-                }
-            }
+            let (offset_, size_) = func(buffer_mut_view, &mut child.1);
+            offset = offset.add(offset_);
+            size = size_;
         }
+
+        buffer
     }
 }
 
@@ -101,8 +139,10 @@ impl View for DockPanel {
     }
     fn render(&mut self, buf: &mut BufferMutView) {
         if self.buffer_cache.is_none() || self.buffer_cache.as_ref().unwrap().size() != buf.size() {
-            self.buffer_cache = Some(Buffer::new(buf.size()));
-            self.render_child();
+            self.buffer_cache = Some(DockPanel::render_child(
+                Buffer::new(buf.size()),
+                &mut self.childs,
+            ));
         }
         buf.write_buffer(self.buffer_cache.as_ref().unwrap());
     }
